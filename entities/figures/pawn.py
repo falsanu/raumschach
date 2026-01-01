cd55f406c13671615c95e910c6b0ad50beeb0079
Jan Fanslau <jan@falsanu.de>
2026-01-01 14:40:42 +0100

    added movement for pawns from different sites

--- entities/figures/pawn.py
+++ entities/figures/pawn.py
@@ -1,3 +1,4 @@
+from settings import *
 from entities.figures.figure import Figure
 import pygame.math
 
@@ -5,15 +6,24 @@ class Pawn(Figure):
     def __init__(self, board, box, team):
         super().__init__(board, box, team)
         self.type = "Pawn"
+
+
         self.movement_vector = [
                                 pygame.math.Vector3(0, 1, 0),   # Bewegung eins hoch
                                 pygame.math.Vector3(0, 2, 0),   # Bewegung zwei hoch
                                 pygame.math.Vector3(0, -1, 0),  # Bewegung eins runter
                                 pygame.math.Vector3(0, -2, 0),  # Bewegung zwei runter
-                                pygame.math.Vector3(0, 0, 1),   # Bewegung eins vor
-                                pygame.math.Vector3(0, 0, 2),   # Bewegung zwei vor
                             ]
-    
+        
+        if self.team == TEAM_WHITE:
+                                self.movement_vector.append(pygame.math.Vector3(0, 0, 1))   # Bewegung eins vor
+                                self.movement_vector.append(pygame.math.Vector3(0, 0, 2))    # Bewegung zwei vor
+
+        if self.team == TEAM_BLACK:
+                                self.movement_vector.append(pygame.math.Vector3(0, 0, -1))   # Bewegung eins vor
+                                self.movement_vector.append(pygame.math.Vector3(0, 0, -2))    # Bewegung zwei vor
+            
+
         self.hit_vector = [
                                 pygame.math.Vector3(-1, 0, 1),  # Schlagen eins vor, eins link
                                 pygame.math.Vector3(1, 0, 1),   # Schlagen eins vor, eins rechts
@@ -25,7 +35,11 @@ class Pawn(Figure):
                                 pygame.math.Vector3(1, -1, 1),  # Schlagen eins runter, eins rechts
 
                             ]
+        if self.team == TEAM_BLACK:
+            for v in self.hit_vector:
+                v.z *= -1
+        
+
         self.label = "P"
         self.highlight_box()
-    
     
